667b75d86dc80b5cec4c2a0d7996343e6e2dc23f
zimyand <zimyand@zimyand-msiwind.(none)>
2010-02-03 23:25:04 +0200

    t2_auth

--- djangotest/simple/views.py
+++ djangotest/simple/views.py
@@ -1,9 +1,30 @@
 from django.template import loader, Context
 from django.http import HttpResponse
 from djangotest.simple.models import UserInfo
+from django.contrib.auth import authenticate, login
 
 def main(request):
+    error = []
+    if (request.POST.has_key('username') and request.POST.has_key('userpass')):
+        error = loginUser(request)
+        
     info = UserInfo.objects.get()
+    
+    data = {'info': info}
+    if error:
+        data['error'] = error
+    if request.user.is_authenticated():
+        data['user_display_name'] = request.user.first_name+" "+request.user.last_name
+    c = Context(data)
     t = loader.get_template('main.html')
-    c = Context({'info': info})
     return HttpResponse(t.render(c))
+
+def loginUser(request):
+    username = request.POST['username']
+    password = request.POST['userpass']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return
+    else:
+        return {"type": 1, "value": "pass incorrect"}
