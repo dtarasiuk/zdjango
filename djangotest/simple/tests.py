fcfcbf342ca93dfff0f4954d2bb9e2c0f79a27a9
zimyand <zimyand@zimyand-msiwind.(none)>
2010-02-16 03:05:37 +0200

    t2 tests

--- djangotest/simple/tests.py
+++ djangotest/simple/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from models import UserInfo
+from django.contrib.auth.models import User
 
 class ModelTest(TestCase):
     def test_fixtures(self):
@@ -10,3 +11,22 @@ class ModelTest(TestCase):
         response = self.client.get('/simple/')
         self.failUnlessEqual(response.status_code, 200)
         self.assertContains(response, 'Name:')
+
+    def test_correct_auth(self):
+        User.objects.create_user(username="zimyand", email='zimyand@gmail.com', password="123123z")
+        post_data = {
+            'username': 'zimyand',
+            'userpass': '123123z',
+        }
+        response = self.client.post('/simple/', post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello,")
+
+    def test_incorrect_auth(self):
+        post_data = {
+            'username': 'incorrectuser',
+            'userpass': 'incorrectpass',
+        }
+        response = self.client.post('/simple/', post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Hello,")
