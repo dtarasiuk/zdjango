09307a04a95cdfbd986af898ee20f67484099529
zimyand <zimyand@zimyand-msiwind.(none)>
2010-02-23 03:19:44 +0200

    t6 realisation

--- djangotest/simple/models.py
+++ djangotest/simple/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.forms import ModelForm
+from django.forms.extras.widgets import SelectDateWidget
+from django import forms
 
 
 class UserInfo(models.Model):
     name = models.CharField(max_length=150)
     surname = models.CharField(max_length=150)
+    birthday = models.DateField()
     about = models.TextField(max_length=150)
     contacts = models.TextField(max_length=150)
 
@@ -13,5 +16,6 @@ class Counter(models.Model):
     datetime = models.DateTimeField(auto_now=True)
 
 class UserInfoForm(ModelForm):
+    birthday = forms.DateField(widget=SelectDateWidget())
     class Meta:
         model = UserInfo
