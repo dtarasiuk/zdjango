ebbcbe20474d381494d8ebf71603f1a67639f325
zimyand <zimyand@zimyand-desktop.(none)>
2010-03-18 12:48:18 +0200

    remove junk in comments

--- djangotest/simple/urls.py
+++ djangotest/simple/urls.py
@@ -2,12 +2,7 @@ from django.conf.urls.defaults import *
 from djangotest.simple.views import main, logoutUser
 
 urlpatterns = patterns('',
-    # Example:
     (r'^$', main),
     (r'^logout/$',logoutUser)
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 )
